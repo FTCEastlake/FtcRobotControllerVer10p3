98555c14c26ee2678158a99b3485f7fdbb43c32d
Frank Nguyen <rf2499@hotmail.com>
2025-08-10 13:46:53 -0700

    Added EggConfigurations

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Egg/EggConfigurations.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.Egg;
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+
+@Config
+public class EggConfigurations {
+
+    public static RevHubOrientationOnRobot.LogoFacingDirection HUB_LOGO_FACING_DIR = RevHubOrientationOnRobot.LogoFacingDirection.UP;
+    public static RevHubOrientationOnRobot.UsbFacingDirection HUB_USB_FACING_DIR = RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
+    public static double MAX_DRIVE_SPEED = 0.5; // max drive speed should NOT exceed 1.0
+
+    public EggConfigurations() {
+        // Nothing to do here
+    }
+
+
+    public RevHubOrientationOnRobot getHubOrientation() {
+        return new RevHubOrientationOnRobot(HUB_LOGO_FACING_DIR, HUB_USB_FACING_DIR);
+    }
+
+}
